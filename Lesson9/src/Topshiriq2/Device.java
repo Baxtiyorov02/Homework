8c1ada3f89055e3b1fdd0b4a78b525d5be4a9b8f
Baxtiyorov02 <144550960+Baxtiyorov02@users.noreply.github.com>
2023-12-16 15:50:29 +0500

    Lesson9 ni ikkinchi misoli
    Done

--- Lesson9/src/Topshiriq2/Device.java
+++ Lesson9/src/Topshiriq2/Device.java
@@ -1,5 +1,7 @@
 package Topshiriq2;
 
+import java.util.Arrays;
+
 public class Device {
 
     private int DeviceMemory;
@@ -9,6 +11,38 @@ public class Device {
         DeviceMemory = deviceMemory;
     }
 
-    private  boolean isExist
+    private  boolean isExist(String appName){
+        boolean isExist=false;
+        for (int i = 0; i <apps.length ; i++) {
+            if (appName==apps[i].getAppName()) isExist=true;
+        }
+        return isExist;
+    }
+    public  boolean install(App app){
+        if (!isExist(app.getAppName())){
+            App[] newApps= new App[apps.length+1];
+            for (int i = 0; i <apps.length ; i++) {
+                newApps[i]=apps[i];
+                }
+            newApps[apps.length]=app;
+            apps=newApps;
+            return true;
+        }
+        else return false;
+    }
+    public  void showInfo(){
+        System.out.println(Arrays.toString(apps));
+    }
+
+    public static void main(String[] args) {
+        App app=new App("Click",38);
+        App app1=new App("Uzum",26);
+        Device device=new Device(3200);
+        System.out.println(device.install(app));
+        System.out.println(device.install(app1));
+        device.showInfo();
+
+    }
+
 
 }
