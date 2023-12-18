9e281c06b072437c403c27c1655fd3991628c290
Baxtiyorov02 <144550960+Baxtiyorov02@users.noreply.github.com>
2023-12-18 22:57:17 +0500

    Done

---
+++ Lesson_10/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random=new Random(10);
+        int a= random.nextInt(20);
+        System.out.println(a);
+
+    }
+}
